0dd9531aa5baed57d29db77ead20d05236ef07fb
Yash Dave <yashdave@Yashs-MacBook-Air.local>
2020-12-20 11:34:57 -0500

    Message

---
+++ Idk.java
@@ -0,0 +1,19 @@
+
+public abstract class Idk {
+	
+	private int a;
+	private int b;
+
+	Idk() {
+		this.a = 1;
+		this.b = 2;
+	}
+	
+	public static void main(String[] args) {
+		Idk hello = new Idk();
+		System.out.println(hello);
+		System.out.println("My bad")
+
+	}
+
+}
